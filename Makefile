6f658b591b743a5f55e79997dfc07fb66877a763
glitchub <glitchub@users.noreply.github.com>
2019-03-15 17:22:25 -0700

    Add config file, more docs, clean up

--- Makefile
+++ Makefile
@@ -1,19 +1,36 @@
 # install/build various repos needed by pionic 
 
-ifeq (${USER},root)
-default:; @echo Must not be root!; false
-else
-
-# note must escape the colon
+# git repos to fetch, note we must escape the ":"
 repos=https\://github.com/glitchub/beacon
 repos+=https\://github.com/glitchub/runfor
 repos+=https\://github.com/glitchub/fbput
 repos+=https\://github.com/glitchub/FM_Transmitter_RPi3
 
+# apt packages to install
 packages=sox graphicsmagick
 
-default: ${repos} ${packages} .gitignore
-	sudo sed -i "/pionic/d; /^exit/i /home/pi/pionic/pionic.sh start" /etc/rc.local
+# system service to disable
+disable=avahi-daemon
+
+# Text to append to dhcpcd.conf
+# The first line must contain the string 'pionic'
+define DHCPCD
+# This is installed by pionic Makefile, do not edit! Changes to dhcpcd.conf must be above this line!
+allowinterfaces eth0
+noipv6
+noipv4ll
+endef
+export DHCPCD
+
+# Make sure we're on RPi and not root
+default: $(if $(shell grep Raspberry /etc/rpi-issue),,$(error Can only be run on Raspberry PI))
+default: $(if $(filter root,${USER}),$(error Must not be run as root))
+
+default: ${repos} ${packages} ${disable} .gitignore
+	sudo sed -i '/pionic/d; /^exit/i /home/pi/pionic/pionic.sh start' /etc/rc.local
+	sudo sed -i '/pionic/Q' /etc/dhcpcd.conf
+	echo "$$DHCPCD" | sudo bash -c 'cat >> /etc/dhcpcd.conf'
+	sync
 
 .PHONY: ${repos}
 ${repos}:
@@ -24,6 +41,10 @@ ${repos}:
 ${packages}:
 	sudo apt install $@
 
+.PHONY: ${disable}
+${disable}:
+	sudo systemctl disable $@  
+
 .PHONY: .gitignore
 .gitignore:
 	echo $@ > $@    
@@ -32,6 +53,6 @@ ${packages}:
 clean:
 	sudo ./pionic.sh stop
 	sudo sed -i "/pionic/d" /etc/rc.local
+	sudo sed -i '/pionic/Q' /etc/dhcpcd.conf
 	rm -rf $(notdir ${repos})
-
-endif
+	sync
--- pionic.sh
+++ pionic.sh
@@ -2,76 +2,84 @@
 # This runs at boot, start test station operation
 # It can also be run manually to restart all services
 
-# Factory server address and https port, advertised in the beacon, change as
-# needed
-factory_server=10.2.3.4:8000
-
-# DUT default address, must be /24 network and not ending with .1
-dut=192.168.111.10
-
 set -ue; trap 'echo $0: line $LINENO: exit status $? >&2' ERR
 
 die() { echo "$*" >&2; exit 1; }
-((UID==0)) || die "Must be root!"
 
+grep -q Raspberry /etc/rpi-issue || die "Can only be run on Raspberry PI"
+((UID==0)) || die "Must be run as root"
+
+here=${0%/*}
+
+lookup() { awk 'BEGIN{X=1} {gsub(/[ \t]+/,"");if($1=="'$1'"){print $2; X=0; exit}} END{exit X}' FS== $here/pionic.cfg; }
+
+# return ip address for interface $1 or "" 
 ipaddr() { ip -4 -o a show dev $1 | awk '{print $4}'; }
-macaddr() { cat /sys/class/net/$1/address; }
-carrier() { cat /sys/class/net/$1/carrier; }
 
-netstat()
+# return true if interface $1 is up and has ip address, false if not, sets $stat1 and $stat2
+isup()
 {
-    if mac=$(macaddr $1); then
-        stat=$(ipaddr $1)
-        [[ $stat ]] && return 0
-        (($(carrier $1))) && stat="NO IP" || stat="UNPLUGGED"
+    if stat1=$(cat /sys/class/net/$1/address); then
+        stat2=$(ipaddr $1)
+        ! [[ $stat2 ]] || return 0
+        (($(cat /sys/class/net/$1/carrier))) && stat2="NO IP" || stat2="UNPLUGGED"
         return 1
     fi
-    mac="NO DEVICE"
-    stat=""
+    stat1="NO DEVICE"
+    stat2=""
     return 0
 }
 
 case "${1:-start}" in
     start)
-        beacon=~pi/pionic/beacon/beacon; [ -x $beacon ] || die "Need executable $beacon"
-        cgiserver=~pi/pionic/cgiserver; [ -x $cgiserver ] || die "Need executable $cgiserver"
+        # extract interesting configuration from pionic.cfg
+        for key in use_beacon factory_ip pionic_ip dut_ip bind_cgi; do
+            val=$(lookup $key) || die "Must define '$key' in pionic.cfg"
+            declare $key=$val
+        done    
 
-        # DUT is attached to eth1 via usb ethernet dongle
-        for t in {1..20}; do
-            if ip l set eth1 up; then
-                # takes a static IP on the DUT's subnet
-                ip a a dev eth1 ${dut%.*}.1/24
+        # eth1 attaches to dut via usb ethernet dongle
+        # bring it up and assign static IP
+        SECONDS=0
+        echo "Waiting for eth1 to come up"
+        while true; do
+            if ip l set dev eth1 up 2>/dev/null; then
+                ip a add dev eth1 $pionic_ip
                 sleep 1
 
-                # Start the beacon server on eth1, the payload contains the factory
-                # server address and port.
-                $beacon send eth1 $factory_server &
+                # Maybe start the beacon server on eth1, the payload contains the factory server address
+                if ((use_beacon)); then
+                    $here/beacon/beacon send eth1 $factory_ip &
+                    disown
+                fi    
 
-                # Start cgi server, bind to eth1
-                #$cgiserver -b 172.31.255.1 -p 80 -d cgi &
-                $cgiserver -p 80 -d ~pi/pionic/cgi &
+                # Start cgi server, maybe bind to eth1
+                ((bind_cgi)) && bind="-b ${pionic_ip%/*}" || bind=""
+                $here/cgiserver $bind -p 80 -d ~pi/pionic/cgi &
+                disown
+
                 break
             fi
-            sleep 1
+            ((SECONDS < 10)) || break
+            sleep .5
         done
 
-        # Factory is attached to eth0, wait for an address via dhcp
-        for t in {1..20}; do
+        # eth0 attaches to the factory 
+        # wait for it to get an address from dhcp
+        echo "Waiting for eth0 to come up"
+        while true; do
             if [[ $(ipaddr eth0) ]]; then
-
                 # NAT the DUT to the factory
                 echo 1 > /proc/sys/net/ipv4/ip_forward
                 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-                #sudo iptables -A FORWARD -i eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
                 # Forward port 2222 from the factory to DUT's ssh
-                iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 2222 -j DNAT --to $dut:22
-                # iptables -A FORWARD -i eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+                iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 2222 -j DNAT --to $dut_ip:22
 
                 break
             fi
-            echo "Waiting for eth1..."
-            sleep 1
+            ((SECONDS < 10)) || break
+            sleep .5
         done
 
         $0 show
@@ -93,28 +101,27 @@ case "${1:-start}" in
         ;;
 
     show)
-        # We'll use the console for test output
+        # disable console output
 	setterm --cursor off > /dev/tty1
         echo 0 > /sys/class/vtconsole/vtcon1/bind
 	dmesg -n 1
 
         ok=1
-        netstat eth0 || ok=0
-        mac0=$mac stat0=$stat
-        netstat eth1 || ok=0
-        mac1=$mac stat1=$stat
+        isup eth0 || ok=0; eth0_stat1=$stat1; eth0_stat2=$stat2
+        isup eth1 || ok=0; eth1_stat1=$stat1; eth1_stat2=$stat2
         pgrep -f cgiserver &>/dev/null && cup=1 || { cup=0; ok=0; }
-        pgrep -f beacon &>/dev/null && bup=1 || { bup=0; ok=0; }
+        use_beacon=$(lookup use_beacon)
+        ((use_beacon)) && { pgrep -f beacon &>/dev/null && bup=1 || { bup=0; ok=0; }; }
 
         {
             ((ok)) && echo Test station started OK || echo TEST STATION DID NOT START
             echo
-            echo "ETH0  : $mac0"
-            [[ $stat0 ]] && echo "        $stat0"
-            echo "ETH1  : $mac1"
-            [[ $stat1 ]] && echo "        $stat1"
+            echo "ETH0  : $eth0_stat1"
+            ! [[ $eth0_stat2 ]] || echo "        $eth0_stat2"
+            echo "ETH1  : $eth1_stat1"
+            ! [[ $eth1_stat2 ]] || echo "        $eth1_stat2"
             ((cup)) && echo CGISRV: OK || echo CGISRV: NOT RUNNING
-            ((bup)) && echo BEACON: OK || echo BEACON: NOT RUNNING
+            ((use_beacon)) && { ((bup)) && echo BEACON: OK || echo BEACON: NOT RUNNING; }
         } | ~pi/pionic/cgi/display command=text fg=white point=50 $( ((ok)) && echo bg=green || echo bg=red)
         ;;
 
